674bc1b0bbf4cc998a8b8f67c9bdcd03633a6360
vance.petitt <vance.petitt@gmail.com>
2022-03-16 11:59:21 -0500

    Made migration.

--- backend/drf_jwt_backend/cars/models.py
+++ backend/drf_jwt_backend/cars/models.py
@@ -4,10 +4,13 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 # <<<<<<<<<<<<<<<<< EXAMPLE FOR STARTER CODE USE <<<<<<<<<<<<<<<<<
-
-
 class Car(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    make = models.CharField(max_length=30)
-    model = models.CharField(max_length=100)
+    make = models.CharField(max_length=255)
+    model = models.CharField(max_length=255)
     year = models.IntegerField()
+
+# class Reply(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+#     text = models.CharField(max_length=255)
