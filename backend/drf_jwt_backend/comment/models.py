18fc2fc518172b66b7ba2a08bdc40e367d80a342
matthew-pryor <pryor.matthew.a@gmail.com>
2022-03-16 15:02:50 -0500

    created comment app + created reply app + created CommentSerializer + created Comment models

---
+++ backend/drf_jwt_backend/comment/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Comment(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    video_id = models.CharField(max_length=999)
+    text = models.CharField(max_length=1200)
+    likes = models.IntegerField()
+    dislikes = models.IntegerField()
+
+# class Reply(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+#     text = models.CharField(max_length=255)
