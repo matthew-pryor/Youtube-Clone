4460d190246b0f19ed6da3c2dbbdd2e4a016fa02
matthew-pryor <pryor.matthew.a@gmail.com>
2022-03-23 14:30:14 -0500

    passed to partner for troubleshooting the displaySearchResultsPage

--- frontend/src/pages/SearchResultsPage/SearchResultsPage.jsx
+++ frontend/src/pages/SearchResultsPage/SearchResultsPage.jsx
@@ -1,43 +1,39 @@
 import React, {useState, useEffect} from "react";
 import DisplaySearchResults from "../../components/DisplaySearchResults/DisplaySearchResults";
 import axios from "axios";
+import { useLocation } from "react-router-dom";
 
 
 
-const SearchResultsPage = () => {
+const SearchResultsPage = (props) => {
 
     const [searchResults, setSearchResults] = useState([]);
+    const {state} = useLocation()
 
-    useEffect(() => {
-      getSearchResults()
-    }, [])
       
-    async function getSearchResults(searchTerm="bob ross"){
-      let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&search?q=${searchTerm}&type=video&key=AIzaSyBuzjiMZRf5Ajpg69rAQjY92YIC18cCjS4`)
-      console.log(response.data);
-      console.log(response.data.items.map((entry) => {
-        return(
-          entry.snippet.thumbnails.high
-        )
-      }))
-      setSearchResults(response.data);
+    async function getSearchResults(){
+      const response = await axios.get(`https://www.googleapis.com/youtube/v3/search?q=${state.searchTerm}&totalResults=5&key=AIzaSyBuzjiMZRf5Ajpg69rAQjY92YIC18cCjS4`)
+      console.log(searchResults);
+      setSearchResults(response.data.items);
     }
     
-
+    useEffect(() => {
+      getSearchResults(state.searchTerm)
+    }, [])
 
     return ( 
         <div>
             <h1>Search Results:</h1>
-            {/* <table>
+            <table>
               <tbody>
-                <tr> */}
                   {searchResults &&
-                    searchResults.map((entry) => (
-                    <img src={`https://i.ytimg.com/vi/${entry.id.videoId}/hqdefault.jpg`}/>
+                    searchResults.map((searchResults) => (
+                      <tr>
+                        <img src={`https://i.ytimg.com/vi/${searchResults.id.videoId}/hqdefault.jpg`}/>
+                      </tr>
                   ))}
-                {/* </tr>
               </tbody>
-            </table> */}
+            </table>
         </div>
      );
 }
