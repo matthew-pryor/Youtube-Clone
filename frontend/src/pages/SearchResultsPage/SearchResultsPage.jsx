1a002cd6335c17323ec1816e31fbf86784dfdabb
matthew-pryor <pryor.matthew.a@gmail.com>
2022-03-23 17:09:14 -0500

    troubleshooting displayPage

--- frontend/src/pages/SearchResultsPage/SearchResultsPage.jsx
+++ frontend/src/pages/SearchResultsPage/SearchResultsPage.jsx
@@ -11,17 +11,10 @@ const SearchResultsPage = (props) => {
     const {state} = useLocation()
 
       
-<<<<<<< HEAD
     async function getSearchResults(){
       const response = await axios.get(`https://www.googleapis.com/youtube/v3/search?q=${state.searchTerm}&totalResults=5&key=AIzaSyBuzjiMZRf5Ajpg69rAQjY92YIC18cCjS4`)
       console.log(searchResults);
       setSearchResults(response.data.items);
-=======
-    async function getSearchResults(searchTerm=''){
-      let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&search?q=${searchTerm}&type=video&key=AIzaSyBuzjiMZRf5Ajpg69rAQjY92YIC18cCjS4`)
-      console.log(response.data);
-      setSearchResults(response.data);
->>>>>>> acaed429bb0dbc9c059bb9269fc44b3e47654835
     }
     
     useEffect(() => {
@@ -33,21 +26,12 @@ const SearchResultsPage = (props) => {
             <h1>Search Results:</h1>
             <table>
               <tbody>
-<<<<<<< HEAD
                   {searchResults &&
                     searchResults.map((searchResults) => (
                       <tr>
                         <img src={`https://i.ytimg.com/vi/${searchResults.id.videoId}/hqdefault.jpg`}/>
                       </tr>
                   ))}
-=======
-                <tr>
-                  {searchResults &&
-                    searchResults.map((searchResults) => (
-                    <img src={`https://i.ytimg.com/vi/${searchResults.id.videoId}/hqdefault.jpg`}/>
-                  ))}
-                </tr>
->>>>>>> acaed429bb0dbc9c059bb9269fc44b3e47654835
               </tbody>
             </table>
         </div>
